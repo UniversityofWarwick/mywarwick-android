59cc3abb9e1579e576f5e8399834a0ffe7c8a77b
Nick Howes <n.howes@warwick.ac.uk>
2017-04-19 11:58:41 +0100

    NEWSTART-649
    Don't try to setup edit button before it exists.
    
    Wrote a test, though it has to run against a device or emulator so not easily runnable by Bamboo at the moment.

---
+++ app/src/androidTest/java/uk/ac/warwick/my/app/activities/NotificationIntentRule.java
@@ -0,0 +1,20 @@
+package uk.ac.warwick.my.app.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+/**
+ * Adds a "from" extra data to the loading intent, which is enough to
+ * trigger the "open straight into Notifications" behaviour.
+ */
+class NotificationIntentRule<T extends Activity> extends ActivityTestRule<T> {
+    public NotificationIntentRule(Class<T> activityClass) {
+        super(activityClass);
+    }
+
+    @Override
+    protected Intent getActivityIntent() {
+        return super.getActivityIntent().putExtra("from", "a_notification");
+    }
+}
