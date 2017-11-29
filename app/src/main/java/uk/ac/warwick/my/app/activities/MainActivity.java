e4157c3b744496c92acb467c3bc225c6344048d8
Kai Lan <k.lan@warwick.ac.uk>
2017-11-29 13:09:45 +0000

    NEWSTART-1258 update every 60 sec

--- app/src/main/java/uk/ac/warwick/my/app/activities/MainActivity.java
+++ app/src/main/java/uk/ac/warwick/my/app/activities/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity implements OnTabSelectListen
                 public void run() {
                     new EventFetcher(getApplicationContext()).updateEvents();
                 }
-            }, 0, 10, TimeUnit.SECONDS);
+            }, 0, 60, TimeUnit.SECONDS);
         }
     }
 
