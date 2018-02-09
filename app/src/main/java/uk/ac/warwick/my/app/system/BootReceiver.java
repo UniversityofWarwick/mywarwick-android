94c7d9d917e94a0522fbd9ee7d7489b4ed39ed0d
Nick Howes <n.howes@warwick.ac.uk>
2018-02-09 14:00:30 +0000

    Guard BootReceiver.
    Lint suggested that although only the system can send the receiver the boot intent action, any app could send _some_ intent to the receiver, so we need to check what action we're actually receiving.

--- app/src/main/java/uk/ac/warwick/my/app/system/BootReceiver.java
+++ app/src/main/java/uk/ac/warwick/my/app/system/BootReceiver.java
@@ -3,14 +3,22 @@ package uk.ac.warwick.my.app.system;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import uk.ac.warwick.my.app.services.DownloadScheduler;
 import uk.ac.warwick.my.app.services.EventNotificationScheduler;
 
 public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        new DownloadScheduler(context).scheduleRepeatingDownload();
-        new EventNotificationScheduler(context).scheduleNextNotification();
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.i(TAG, "Triggering boot actions");
+            new DownloadScheduler(context).scheduleRepeatingDownload();
+            new EventNotificationScheduler(context).scheduleNextNotification();
+        } else {
+            Log.w(TAG, "Ignoring intent with action " + intent.getAction());
+        }
     }
 }
