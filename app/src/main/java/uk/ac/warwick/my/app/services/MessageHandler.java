536065b98493a5cf2782a451a64bf2570fda315c
Nic Duke <n.duke@warwick.ac.uk>
2018-03-07 15:01:23 +0000

    NEWSTART-1367 remove unused import

--- app/src/main/java/uk/ac/warwick/my/app/services/MessageHandler.java
+++ app/src/main/java/uk/ac/warwick/my/app/services/MessageHandler.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.messaging.FirebaseMessagingService;
