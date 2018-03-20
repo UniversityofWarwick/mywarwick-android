5ae2c8a35d936479211bcc762ff2cd898c97ed2d
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-03-20 10:23:21 +0000

    NEWSTART-1402 - Manually handle local time zones

--- app/src/main/java/uk/ac/warwick/my/app/utils/ISO8601RoughParser.java
+++ app/src/main/java/uk/ac/warwick/my/app/utils/ISO8601RoughParser.java
@@ -4,22 +4,60 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Parses the datetimes that we need to parse, but
  * not everything in ISO8601, which is why it's "rough".
  *
  * Like SimpleDateFormat, this is _not_ thread-safe.
+ *
+ * Android API < 24 doesn't support X in format, so manually support Zulu, GMT, and BST
  */
 public class ISO8601RoughParser {
-    private final SimpleDateFormat[] formats = new SimpleDateFormat[] {
-        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.UK),
-        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.UK),
+    private final SimpleDateFormat[] zuluFormats = new SimpleDateFormat[] {
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.UK),
     };
+
+    private final SimpleDateFormat[] gmtFormats = new SimpleDateFormat[] {
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+00:00'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'+00:00'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+0000'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'+0000'", Locale.UK),
+    };
+
+    private final SimpleDateFormat[] bstFormats = new SimpleDateFormat[] {
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+01:00'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'+01:00'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+0100'", Locale.UK),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'+0100'", Locale.UK),
+    };
+
+    public ISO8601RoughParser() {
+        TimeZone utcTZ = TimeZone.getTimeZone("UTC");
+        for (SimpleDateFormat format : zuluFormats) {
+            format.setTimeZone(utcTZ);
+        }
+    }
 
     public Date parse(String input) throws ParseException {
         ParseException lastException = null;
-        for (SimpleDateFormat format : formats) {
+        for (SimpleDateFormat format : zuluFormats) {
+            try {
+                return format.parse(input);
+            } catch (ParseException e) {
+                lastException = e;
+            }
+        }
+        for (SimpleDateFormat format : gmtFormats) {
+            try {
+                return format.parse(input);
+            } catch (ParseException e) {
+                lastException = e;
+            }
+        }
+        for (SimpleDateFormat format : bstFormats) {
             try {
                 return format.parse(input);
             } catch (ParseException e) {
--- app/src/test/java/uk/ac/warwick/my/app/services/EventFetcherTest.java
+++ app/src/test/java/uk/ac/warwick/my/app/services/EventFetcherTest.java
@@ -34,13 +34,45 @@ public class EventFetcherTest {
     }
 
     @Test
-    public void buildObject() throws Exception {
+    public void buildObjectZulu() throws Exception {
         JSONObject json = new JSONObject()
                 .put("id", "1")
                 .put("type", "meeting")
                 // Normally has millis; just testing that they're optional.
                 .put("start", "2017-10-13T12:00:00Z")
                 .put("end", "2017-10-13T13:00:00.123Z");
+        Event event = fetcher.buildEvent(json);
+        assertEquals(Date.from(Instant.parse("2017-10-13T12:00:00.000Z")), event.getStart());
+        assertEquals(Date.from(Instant.parse("2017-10-13T13:00:00.123Z")), event.getEnd());
+        assertEquals("", event.getTitle());
+        assertEquals("1", event.getServerId());
+        assertEquals("meeting", event.getType());
+    }
+
+    @Test
+    public void buildObjectGMT() throws Exception {
+        JSONObject json = new JSONObject()
+                .put("id", "1")
+                .put("type", "meeting")
+                // Normally has millis; just testing that they're optional.
+                .put("start", "2017-11-13T12:00:00+00:00")
+                .put("end", "2017-11-13T13:00:00.123+0000");
+        Event event = fetcher.buildEvent(json);
+        assertEquals(Date.from(Instant.parse("2017-11-13T12:00:00.000Z")), event.getStart());
+        assertEquals(Date.from(Instant.parse("2017-11-13T13:00:00.123Z")), event.getEnd());
+        assertEquals("", event.getTitle());
+        assertEquals("1", event.getServerId());
+        assertEquals("meeting", event.getType());
+    }
+
+    @Test
+    public void buildObjectBST() throws Exception {
+        JSONObject json = new JSONObject()
+                .put("id", "1")
+                .put("type", "meeting")
+                // Normally has millis; just testing that they're optional.
+                .put("start", "2017-10-13T13:00:00+01:00")
+                .put("end", "2017-10-13T14:00:00.123+0100");
         Event event = fetcher.buildEvent(json);
         assertEquals(Date.from(Instant.parse("2017-10-13T12:00:00.000Z")), event.getStart());
         assertEquals(Date.from(Instant.parse("2017-10-13T13:00:00.123Z")), event.getEnd());
